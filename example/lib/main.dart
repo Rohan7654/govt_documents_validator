d9bb09f5c9de38c587334baf1b65a1e10c1edb4c
Rohan <43801290+ROHAN@users.noreply.github.com>
2021-02-28 01:54:41 +0530

    Added Example Code and Updated Main Code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,9 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
       theme: ThemeData(
-        brightness: Brightness.dark,
+        brightness: Brightness.light,
       ),
     );
   }
@@ -36,25 +37,16 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Form Validation"),
-        leading: Icon(Icons.filter_vintage),
+        title: Text("Form-Validation In Flutter"),
+        elevation: 2.0,
       ),
-      //body
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(20.0),
         //form
         child: Form(
           key: _formKey,
           child: Column(
             children: <Widget>[
-              Text(
-                "Form-Validation In Flutter ",
-                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-              ),
-              //styling
-              SizedBox(
-                height: MediaQuery.of(context).size.width * 0.1,
-              ),
               TextFormField(
                 cursorColor: Colors.green,
                 decoration: InputDecoration(labelText: 'Aadhar Number'),
@@ -72,14 +64,16 @@ class _HomePageState extends State<HomePage> {
               ),
               //text input
               RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
                 padding: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 15.0,
+                  vertical: 5.0,
+                  horizontal: 10.0,
                 ),
                 child: Text(
                   "Submit",
                   style: TextStyle(
-                    fontSize: 24.0,
+                    fontSize: 20.0,
                   ),
                 ),
                 onPressed: () => _submit(),
